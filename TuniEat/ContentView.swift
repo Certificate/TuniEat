f54d8be9625cfe4671ade1c823233e1327d24166
Valtteri Vuori <valtteri.vuori@live.fi>
2019-12-10 17:18:40 +0200

    Initial Commit

---
+++ TuniEat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TuniEat
+//
+//  Created by Valtteri Vuori on 10.12.2019.
+//  Copyright © 2019 Valtteri Vuori. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
