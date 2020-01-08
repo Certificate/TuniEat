57ec8b06071e6bfda5ff4133924b371535950bcf
Valtteri Vuori <valtteri.vuori@live.fi>
2020-01-08 10:40:58 +0200

    feat: Add a navigation bar to the view

--- TuniEat/AppDelegate.swift
+++ TuniEat/AppDelegate.swift
@@ -18,7 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        window?.rootViewController = CustomTabBarController()
+        let navbar = UINavigationController()
+        let mainView = CustomTabBarController()
+        navbar.viewControllers = [mainView]
+        
+        window?.rootViewController = navbar
         
         // Override point for customization after application launch.
         return true
